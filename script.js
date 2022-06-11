2f7fce64cc9db5ea588c2225b462844f2b65c59e
Lawson Nuland <lawson.nuland@gmail.com>
2022-06-11 13:48:23 -0400

    Added deletion, added styling

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ Book.prototype.info = function () {
 
 const bookie = new Book("lotr", "jrr", 300, "unread");
 library.push(bookie);
-console.log(bookie.info())
+//console.log(bookie.info())
 
 //Displaying the library
 const libraryDisplay = document.querySelector('.librarydisplay');
@@ -21,16 +21,39 @@ function updateLibrary () {
   while (libraryDisplay.firstChild) {
     libraryDisplay.removeChild(libraryDisplay.firstChild);
   }
+  let bookCount = 0;
   library.forEach(element => {
     let card = document.createElement('div');
     card.classList.add('card');
+    card.setAttribute("data-index", bookCount);
     card.innerHTML = `
+    <div>
     <p>Title: ${element.name}</p>
     <p>Author: ${element.author}</p>
     <p>Pagecount: ${element.pages}</p>
     <p>Read: ${element.read}</p>
+    </div>
+    <div class="buttons">
+     <button class="delete" data-index="${bookCount}">Delete</button>
+     <button class="readtoggle">Read/Unread</button>
+    </div>
     `
     libraryDisplay.appendChild(card);
+    bookCount++;
+  });
+
+  let deleteButtons = document.querySelectorAll('.delete');
+  deleteButtons.forEach((button) => {
+    button.addEventListener('click', ()=> {
+      deleteBook(button.getAttribute("data-index"));
+    }); 
+  });
+
+  let readButtons = document.querySelectorAll('.readtoggle');
+  readButtons.forEach((button) => {
+    button.addEventListener('click', ()=> {
+      toggleRead();
+    }); 
   });
 }
 
@@ -42,11 +65,19 @@ addButton.addEventListener('click', () => {
   addBookToLibrary();
 });
 
-
 function addBookToLibrary() {
   let book = new Book(document.querySelector('#title').value,document.querySelector('#author').value,document.querySelector('#pagecount').value,document.querySelector('#readbool').value);
-  console.log(book.info);
+  //console.log(book.info);
   library.push(book);
-  console.log(library);
+  //console.log(library);
   updateLibrary();
+}
+
+function deleteBook(target) {
+  library.splice(target,1);
+  updateLibrary();
+}
+
+function toggleRead() {
+  console.log("read!");
 }
